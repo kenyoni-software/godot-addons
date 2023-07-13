612d8bf1cb2ece1c7108d4ad058ba32f10017490
iceflower <iceflower@gmx.de>
2023-07-13 18:20:56 +0200

    [plugin|hpp] Fix static typing

--- addons/hide_private_properties/inspector_plugin.gd
+++ addons/hide_private_properties/inspector_plugin.gd
@@ -3,7 +3,7 @@ extends EditorInspectorPlugin
 var editor_interface: EditorInterface
 
 func _can_handle(object: Object) -> bool:
-    var scene_path: String = object.get("scene_file_path")
+    var scene_path: Variant = object.get("scene_file_path")
     return scene_path != null && scene_path != "" && object != self.editor_interface.get_edited_scene_root()
 
 func _parse_property(object: Object, type: Variant.Type, name: String, hint_type: PropertyHint, hint_string: String, usage_flags: int, wide: bool) -> bool:
