8c214d0ce87cca554a61a740722b969eeeb45787
iceflower <iceflower@gmx.de>
2023-07-18 21:01:41 +0200

    [plugin|cto] Fix can revert property

--- addons/custom_theme_overrides/theme_overrides.gd
+++ addons/custom_theme_overrides/theme_overrides.gd
@@ -11,6 +11,22 @@ class Item:
         self.name = name
         self.type = type
 
+    func full_name() -> String:
+        match self.type:
+            Theme.DATA_TYPE_COLOR:
+                return "theme_override_colors/" + self.name
+            Theme.DATA_TYPE_CONSTANT:
+                return "theme_override_constants/" + self.name
+            Theme.DATA_TYPE_FONT:
+                return "theme_override_fonts/" + self.name
+            Theme.DATA_TYPE_FONT_SIZE:
+                return "theme_override_font_sizes/" + self.name
+            Theme.DATA_TYPE_ICON:
+                return "theme_override_icons/" + self.name
+            Theme.DATA_TYPE_STYLEBOX:
+                return "theme_override_styles/" + self.name
+        return self.name
+
 func _init(overrides: Array[Array]) -> void:
     for item in overrides:
         self._overrides.append(Item.new(item[0], item[1]))
@@ -26,41 +42,35 @@ func theme_property_list(obj: Control) -> Array[Dictionary]:
         "hint_string": "theme_override_"
     }]
     for item in self._overrides:
-        var prop_prefix: String = ""
         var prop_type: Variant.Type
         var prop_hint: PropertyHint
         var prop_hint_string: String
         match item.type:
             Theme.DATA_TYPE_COLOR:
-                prop_prefix = "theme_override_colors/"
                 prop_type = TYPE_COLOR
             Theme.DATA_TYPE_CONSTANT:
-                prop_prefix = "theme_override_constants/"
                 prop_type = TYPE_INT
             Theme.DATA_TYPE_FONT:
-                prop_prefix = "theme_override_fonts/"
                 prop_type = TYPE_OBJECT
                 prop_hint = PROPERTY_HINT_RESOURCE_TYPE
                 prop_hint_string = "Font"
             Theme.DATA_TYPE_FONT_SIZE:
-                prop_prefix = "theme_override_font_sizes/"
                 prop_type = TYPE_INT
             Theme.DATA_TYPE_ICON:
-                prop_prefix = "theme_override_icons/"
                 prop_type = TYPE_OBJECT
                 prop_hint = PROPERTY_HINT_RESOURCE_TYPE
                 prop_hint_string = "Texture2D"
             Theme.DATA_TYPE_STYLEBOX:
-                prop_prefix = "theme_override_styles/"
                 prop_type = Variant.Type.TYPE_OBJECT
                 prop_hint = PROPERTY_HINT_RESOURCE_TYPE
                 prop_hint_string = "StyleBox"
 
+        var full_name: String = item.full_name()
         var usage: int = PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_CHECKABLE
-        if obj.get(prop_prefix + item.name) != null:
+        if obj.get(full_name) != null:
             usage = usage | PROPERTY_USAGE_STORAGE
         props.append({
-            "name": prop_prefix + item.name,
+            "name": full_name,
             "type": prop_type,
             "usage": usage,
             "hint": prop_hint,
@@ -70,6 +80,6 @@ func theme_property_list(obj: Control) -> Array[Dictionary]:
 
 func can_revert(prop_name: StringName) -> bool:
     for item in self._overrides:
-        if item.name == prop_name:
+        if item.full_name() == prop_name:
             return true
     return false
