2eedad894b1f2f09abab397b40c47eb673e8e929
iceflower <iceflower@gmx.de>
2023-09-06 21:14:30 +0200

    [publisher] Tweak zipping
    Fix #1

--- publisher/utils.go
+++ publisher/utils.go
@@ -10,11 +10,13 @@ import (
 
 func zipDir(zw *zip.Writer, src string, dest string) error {
 	return filepath.WalkDir(src, func(path string, d fs.DirEntry, err error) error {
-		if d.IsDir() {
-			return nil
-		}
 		rel, _ := filepath.Rel(src, path)
-		return zipFile(zw, path, filepath.Join(dest, rel))
+		destPath := filepath.Join(dest, rel)
+		if d.IsDir() {
+			_, err := zw.Create(destPath + "/")
+			return err
+		}
+		return zipFile(zw, path, destPath)
 	})
 }
 
