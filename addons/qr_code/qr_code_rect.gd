35e77753ecc7ae07b8ef99e338c8c39c5d1d9b05
Iceflower <iceflower@gmx.de>
2023-09-25 20:34:48 +0200

    [qr] Add return type to method

--- addons/qr_code/qr_code_rect.gd
+++ addons/qr_code/qr_code_rect.gd
@@ -339,7 +339,7 @@ func _property_get_revert(property: StringName) -> Variant:
         _:
             return null
 
-func _get_configuration_warnings():
+func _get_configuration_warnings() -> PackedStringArray:
     if self.auto_module_px_size && self.expand_mode == EXPAND_KEEP_SIZE:
         return ["Do not use auto module px size AND keep size expand mode."]
     return []
