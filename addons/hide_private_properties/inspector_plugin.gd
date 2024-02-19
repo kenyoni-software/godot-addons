cdf4774d8953fe338b1799c15d0b71ec09348c1c
JostMK <jost.klink@gmail.com>
2024-02-19 21:18:41 +0100

    [plugin] Fix property not found warning

--- addons/hide_private_properties/inspector_plugin.gd
+++ addons/hide_private_properties/inspector_plugin.gd
@@ -1,10 +1,19 @@
 extends EditorInspectorPlugin
 
 func _can_handle(object: Object) -> bool:
+    if not _has_property(object, "scene_file_path"):
+        return false
+    
     var scene_path: Variant = object.get("scene_file_path")
     return scene_path != null && scene_path != "" && object != EditorInterface.get_edited_scene_root()
 
 func _parse_property(object: Object, type: Variant.Type, name: String, hint_type: PropertyHint, hint_string: String, usage_flags: int, wide: bool) -> bool:
     if name.begins_with("_"):
         return true
+    return false
+
+func _has_property(object: Object, propertyName: String) -> bool:
+    for property in object.get_property_list():
+        if property.name == propertyName:
+            return true
     return false
