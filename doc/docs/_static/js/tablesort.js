ffafa7e6b315ca947603d38659da8e6a1e566d58
Iceflower <iceflower@gmx.de>
2024-07-31 22:55:20 +0200

    [doc] Improve documentation structure

--- doc/docs/_static/js/tablesort.js
+++
@@ -1,6 +0,0 @@
-document$.subscribe(function() {
-    let tables = document.querySelectorAll("article table:not([class])");
-    tables.forEach(function(table) {
-        new Tablesort(table);
-    });
-});
