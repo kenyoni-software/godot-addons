fc1ba04c18ed75dfb10075bd4b05a22ef6b102cd
Iceflower <iceflower@gmx.de>
2025-01-31 23:00:39 +0100

    [licenses] Simplify example

--- examples/licenses/display_licenses.gd
+++ examples/licenses/display_licenses.gd
@@ -12,26 +12,25 @@ const LicenseContainer := preload("res://examples/licenses/license_container.gd"
 ## Container to display the license details.
 @export var _license_container: LicenseContainer
 
-var licenses: Array[Component] = []
-
 func _ready() -> void:
     # load licenses
     var res: Licenses.LoadResult = Licenses.load(Licenses.get_license_data_filepath())
     if res.err_msg != "":
         return
-    self.licenses = res.components
+    var components: Array[Component] = res.components
     # add engine licenses
-    self.licenses.append_array(Licenses.get_required_engine_components())
-    # sort licenses by name ascending
-    self.licenses.sort_custom(Licenses.new().compare_components_ascending)
+    components.append_array(Licenses.get_required_engine_components())
+    # sort licenses by category and name ascending
+    # if you want the engine license in front of all other, append them in front after sorting
+    components.sort_custom(Licenses.new().compare_components_ascending)
 
     # first example
-    self._populate_tree()
+    self._populate_tree(components)
     # second example
-    self._populate_item_list()
+    self._populate_item_list(components)
 
 ## first example with tree
-func _populate_tree() -> void:
+func _populate_tree(components: Array[Component]) -> void:
     # if an item in the tree is selected, set the license container's component to the selected component
     self._tree.item_selected.connect(self._on_tree_item_selected)
 
@@ -42,12 +41,11 @@ func _populate_tree() -> void:
     category_cache[""] = root
 
     # add each component to the tree
-    for idx: int in range(len(self.licenses)):
-        var component: Component = self.licenses[idx]
+    for component: Component in components:
         var parent: TreeItem = self._create_category_item(category_cache, component.category, root)
         var item: TreeItem = self._tree.create_item(parent)
         item.set_text(0, component.name)
-        item.set_meta("idx", idx)
+        item.set_meta("component", component)
 
 ## create a category item in the tree
 func _create_category_item(category_cache: Dictionary[String, TreeItem], category: String, root: TreeItem) -> TreeItem:
@@ -61,19 +59,18 @@ func _create_category_item(category_cache: Dictionary[String, TreeItem], categor
     return category_item
 
 ## second example with item list
-func _populate_item_list() -> void:
+func _populate_item_list(components: Array[Component]) -> void:
     # if an item in the item list is selected, set the license container's component to the selected component
     self._item_list.item_selected.connect(self._on_item_list_selected)
 
     # add each component to the item list
-    for idx: int in range(len(self.licenses)):
-        var component: Component = self.licenses[idx]
+    for component: Component in components:
         self._item_list.add_item(component.name)
-        self._item_list.set_item_metadata(idx, idx)
+        self._item_list.set_item_metadata(self._item_list.item_count - 1, component)
 
 ## update the license display container to show the selected license
 func _on_tree_item_selected() -> void:
-    self._license_container.set_component(self.licenses[self._tree.get_selected().get_meta("idx")])
+    self._license_container.set_component(self._tree.get_selected().get_meta("component"))
 
 func _on_item_list_selected(idx: int) -> void: 
-    self._license_container.set_component(self.licenses[self._item_list.get_item_metadata(idx)])
+    self._license_container.set_component(self._item_list.get_item_metadata(idx))
