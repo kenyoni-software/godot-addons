1b81ec1f5d8493066263c06983e21285ef7675a5
Iceflower <iceflower@gmx.de>
2025-03-14 21:23:59 +0100

    [glogging] Use path_join

--- addons/glogging/plugin.gd
+++ addons/glogging/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enable_plugin() -> void:
-    self.add_autoload_singleton("GLogging", self.get_script().get_path().get_base_dir() + "/glogging.gd")
+    self.add_autoload_singleton("GLogging", (self.get_script() as Script).get_path().get_base_dir().path_join("glogging.gd"))
 
 func _disable_plugin() -> void:
     self.remove_autoload_singleton("GLogging")
